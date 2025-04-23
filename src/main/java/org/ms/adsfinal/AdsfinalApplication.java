448eff6128e0ca76e3c675bb9834fa21b7af00bd
Eman22S <emanawel2@gmail.com>
2025-04-23 15:50:39 -0500

    Initial commit

---
+++ src/main/java/org/ms/adsfinal/AdsfinalApplication.java
@@ -0,0 +1,13 @@
+package org.ms.adsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsfinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ms/adsfinal/AdsfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.adsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
